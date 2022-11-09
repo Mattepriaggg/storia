ec414488a312036c8e8993deedb81bd9378b56ac
Mattepriagggg <mattepriaggg@gmail.com>
2022-11-09 22:23:07 +0100

    storia 1

---
+++ java.js
@@ -0,0 +1,8 @@
+window.addEventListener('scroll', function() {
+    if(window.scrollY > 260) {
+        document.body.classList.add('scroll-down')
+    } else {
+        this.document.body.classList.remove('scroll-down')
+    }
+} )
+
